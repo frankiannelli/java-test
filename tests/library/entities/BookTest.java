36a85f41415ab32287fab1dd09a9440408b8f6df
frankiannelli <frank.iannelli@outlook.com>
2020-09-26 19:12:59 +1000

    implement book tests

--- tests/library/entities/BookTest.java
+++ tests/library/entities/BookTest.java
@@ -1,15 +1,26 @@
 package library.entities;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BookTest {
+    IBook book;
+    String author = "Frank";
+    String title = "Become a Java dev in 2 weeks";
+    String callNo = "c123";
+    int id = 123;
 
     @BeforeEach
     void setUp() {
+        book = new Book(author, title, callNo, id);
     }
 
     @AfterEach
@@ -17,12 +28,47 @@ class BookTest {
     }
 
     @Test
-    void isAvailable() {
-       fail("not yet implemented");
+    void testAvailableTrueWhenAvailable() {
+        boolean isAvailable = book.isAvailable();
+        assertTrue(isAvailable);
     }
 
     @Test
-    void borrowFromLibrary() {
-        fail("not yet implemented");
+    void testAvailableFalseWhenOnLoan() {
+        IBook.BookState state = IBook.BookState.ON_LOAN;
+        book = new Book(author, title, callNo, id, state);
+        boolean isAvailable = book.isAvailable();
+        assertFalse(isAvailable);
+    }
+
+    @Test
+    void testAvailableFalseWhenDamaged() {
+        IBook.BookState state = IBook.BookState.DAMAGED;
+        book = new Book(author, title, callNo, id, state);
+        boolean isAvailable = book.isAvailable();
+        assertFalse(isAvailable);
+    }
+
+    @Test
+    void testBorrowFromLibraryUpdatesStateWhenAvailable() {
+        book.borrowFromLibrary();
+        boolean isAvailable = book.isAvailable();
+        assertFalse(isAvailable);
+    }
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    @Test
+    void testBorrowFromLibraryThrowsWhenOnLoan() {
+        IBook.BookState state = IBook.BookState.ON_LOAN;
+        book = new Book(author, title, callNo, id, state);
+        try {
+            book.borrowFromLibrary();
+            fail("Should have thrown exception but did not!");
+        } catch (final RuntimeException e) {
+            final String msg = "Book: cannot borrow while book is in state: ON_LOAN";
+            assertEquals(msg, e.getMessage());
+        }
     }
 }
