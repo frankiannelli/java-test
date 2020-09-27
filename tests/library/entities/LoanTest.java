7a9a290968c1caf9e3cb99e0a6a41d18924c17e0
frankiannelli <frank.iannelli@outlook.com>
2020-09-27 11:46:11 +1000

    add unimplemented tests

---
+++ tests/library/entities/LoanTest.java
@@ -0,0 +1,18 @@
+package library.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanTest {
+
+    @Test
+    void commit() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void checkOverDue() {
+        fail("Not yet implemented");
+    }
+}
