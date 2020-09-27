c05796540be0eddb70a804903d5a21ee4117d29a
frankiannelli <frank.iannelli@outlook.com>
2020-09-27 14:50:07 +1000

    initialize tests

--- tests/library/entities/LibraryTestMocks.java
+++ tests/library/entities/LibraryTestMocks.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class TestLibraryMocks {
+class LibraryTestMocks {
     ILibrary library;
 
     @Mock
---
+++ tests/library/entities/PatronTest.java
@@ -0,0 +1,18 @@
+package library.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PatronTest {
+
+    @Test
+    void takeOutLoan() {
+        fail("not yet implemented");
+    }
+
+    @Test
+    void hasOverDueLoans() {
+        fail("not yet implemented");
+    }
+}
